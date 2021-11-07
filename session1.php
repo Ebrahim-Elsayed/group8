8e764a335ae8fa5f46a93326fa64bcc5c876f30a
MrWeber94 <abdelkareemelsayed2014@gmail.com>
2021-11-07 15:31:07 +0200

    session1

---
+++ session1.php
@@ -0,0 +1,144 @@
+<?php 
+
+// 
+# 
+/*   
+   
+*/
+
+/*
+ function to print message 
+ to user 
+*/
+
+  /* function printMessage(){
+    echo 'test Message';
+  }
+*/
+
+
+  //  echo 'php course <br> , nti';
+
+  // print('php Course <br> nti');
+
+
+  //  $name = 'ahmed';
+
+  //  $age = 20;
+   
+  //  $age = 40;
+
+
+
+  //  ECHO $age;   // 40 
+
+
+  //  define('size',40,true);   // case insensitive >>>> false 
+ 
+  //   // echo size;
+
+  //    echo  constant('SIZE');
+
+/*
++ 
+- 
+* 
+/ 
+** 
+%
+*/
+
+ $x = 10;
+ $y = 10;
+
+//    echo ($num1-$num2);   //  sqrt();
+
+
+   //echo $x = $x+$y;  
+  //  echo $x +=$y;    //$x = $x+$y; 
+
+  //  $x = $y ;  $x == $y; 
+    //  var_dump($x === $y);
+    //    != (value)    !==  (value , data type ) 
+  
+  
+    // var_dump($x >= $y);
+     
+
+    // echo  ++$x;     
+
+    // echo  --$x;
+
+/*
+      cond1     op          cond2    result 
+      true      and &&       true     true 
+      true       and         false    false     
+      false      and         true     false     
+      false      and         false    false    
+  */ 
+  
+
+
+  /*
+      cond1       op        cond2    result 
+      true      or ||       true       true
+      true       or         false      true 
+      false      or         true       true   
+      false      or         false      false  
+  */ 
+
+  /*
+      cond1      op         cond2       result 
+      true       xor         true        false
+      true       xor         false       true 
+      false      xor         true        true  
+      false      xor         false       false 
+  */ 
+
+  // !
+  
+  // $con1 = true;
+  // $cond2 = false;
+  // var_dump(!$con1);
+      
+
+  //  $message = "Welcome , ";
+  //  $name = "Root";
+
+  //  echo  $message.$name.' test message ';   // + 
+
+
+
+
+// $status = true;
+
+
+//     if($status == true){
+     
+//       echo "server run";
+
+//     }else{
+//      echo "server down";
+//     }
+
+// $age = 45;
+
+// if($age < 15){
+  
+//   echo 'Offer A';
+
+// }elseif(($age >= 15 ) && ($age < 25) ){
+
+//   echo "Offer B";
+
+// }elseif(($age >= 25 ) && ($age < 40) ){
+
+//   echo "Offer C";
+
+// }else{
+//   echo 'Offer D';
+// }
+
+
+
+?>
